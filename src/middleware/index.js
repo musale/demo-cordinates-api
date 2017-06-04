114794fcc606b539a90184b592414ceae18fdb36
musale <martinmshale@gmail.com>
2017-06-04 20:24:34 +0300

    Added /api/v1/geofence middleware. Checks if the radius and coordinate values have been passed. Raises Error message if not passed. Hooks the db object to the request. I might be onto something here lol :wink:

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,7 +3,20 @@ import { Router } from 'express';
 export default ({ config, db }) => {
 	let routes = Router();
 
-	// additional middleware comes here
+	routes.post('/api/v1/geofence', (req, res, next) =>{
+		const radius = "radius";
+		const coordinate = "coordinate";
+		const request = req.body;
+		const params = [radius, coordinate]
+		for (var param in params) {
+			if (!(params[param] in request)) {
+				res.json(`Error: ${params[param]} is needed!`)
+			}
+		}
+		req.db = db;
+		next();
+
+	})
 
 	return routes;
 }
