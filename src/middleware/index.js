c3165e0dafadfb7e688e15faca988b1a3bfc0bae
musale <martinmshale@gmail.com>
2017-06-05 22:56:49 +0300

    Refactored to remove unused vars and set correct http statuses.

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import isObject from 'isobject';
 
-export default ({ config, db }) => {
+export default ({ db }) => {
 	const routes = Router();
 
 	routes.post('/api/v1/geofence', (req, res, next) =>{
@@ -12,25 +12,22 @@ export default ({ config, db }) => {
 		// check radius and coordinate are supplied
 		for (const param in params) {
 			if (!(params[param] in request)) {
-				res.json({ error: `${params[param]} is needed!` });
+				return res.status(400).json({ error: `${params[param]} is needed!` });
 			}
 		}
 		// check radius is a number
 		if (isNaN(request.radius)) {
-			res.json({ error: `radius should be a number! ${request.radius} is not a number` });
+			return res.status(400).json({ error: `radius should be a number! ${request.radius} is not a number` });
 		}
 		// check coordinate is an array
 		if (!isObject(request.coordinate)) {
-			res.json({ error: `coordinate should be a key: value object! ${request.coordinate} is not a key: value object` });
+			return res.status(400).json({ error: `coordinate should be a key: value object! ${request.coordinate} is not a key: value object` });
 		}
 
 		// check all values in the coordinate are numbers
 		for (const key in request.coordinate) {
-			if (request.coordinate.hasOwnProperty(key)) {
-				console.log(key);
-			}
 			if (isNaN(request.coordinate[key])) {
-				res.json({ error: `values of coordinate should be numbers. ${key}: ${request.coordinate[key]} is not a number.`});
+				return res.status(400).json({ error: `values of coordinate should be numbers. ${key}: ${request.coordinate[key]} is not a number.`});
 			}
 		}
 		req.db = db;
