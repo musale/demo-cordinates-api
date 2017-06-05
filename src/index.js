f618601d7b5489b808eb87b59464bf334f8a96d7
musale <martinmshale@gmail.com>
2017-06-05 22:57:22 +0300

    Refactored app to enable exports for use in testing.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import http from 'http';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -5,13 +6,15 @@ import bodyParser from 'body-parser';
 import initializeDb from './mongodb';
 import middleware from './middleware';
 import api from './api';
-import {credentials} from './config';
+import config from './config';
 
 const app = express();
-const config = credentials.server;
+app.server = http.createServer(app);
 
 // logger
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'test') {
+	app.use(morgan('dev'));
+}
 
 // 3rd party middleware
 app.use(cors({exposedHeaders: config.corsHeaders}));
@@ -19,17 +22,17 @@ app.use(cors({exposedHeaders: config.corsHeaders}));
 app.use(bodyParser.json({limit: config.bodyLimit}));
 
 // connect to db
-initializeDb(db => {
+initializeDb( db => {
 
-  // internal middleware
-  app.use(middleware({config, db}));
+	// internal middleware
+	app.use(middleware({ db }));
 
-  // api router
-  app.use('/api/v1', api({config, db}));
+	// api router
+	app.use('/api/v1', api({ config, db }));
 
-  app.listen(config.port, () => {
-    console.log(`Started on port ${config.port}`);
-  });
+	app.server.listen(process.env.PORT || config.port);
+
+	console.log(`Started on port ${app.server.address().port} ${process.env.NODE_ENV}`);
 });
 
 export default app;
