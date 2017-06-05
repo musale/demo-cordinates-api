06216b95a267f25c4b72692747859ebff4b2a7af
musale <martinmshale@gmail.com>
2017-06-05 18:37:45 +0300

    Added response status for the other API methods.

--- src/api/index.js
+++ src/api/index.js
@@ -11,10 +11,22 @@ export default() => {
     res.json({version});
   });
   api.post('/', (req, res) => {
-    res.status(405).json({ error: 'POST to /api/v1 is not allowed.' });
+    res.status(405).json({ error: 'method POST to /api/v1 is not allowed.' });
   });
 
   api.post('/geofence', geofence);
+  api.get('/geofence', (req, res) => {
+    res.status(405).json({ error: 'method GET on /api/v1/geofence is not allowed.' });
+  });
+  api.put('/geofence', (req, res) => {
+    res.status(405).json({ error: 'method PUT on /api/v1/geofence is not allowed.' });
+  });
+  api.patch('/geofence', (req, res) => {
+    res.status(405).json({ error: 'method PATCH on /api/v1/geofence is not allowed.' });
+  });
+  api.delete('/geofence', (req, res) => {
+    res.status(405).json({ error: 'method DELETE on /api/v1/geofence is not allowed.' });
+  });
   api.post('/geofilter/rectangle', geofilterRectangle);
   api.post('/geofilter/polygon', geofilterPolygon);
 
