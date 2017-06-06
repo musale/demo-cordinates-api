d28c635937c6692dbacd419a3d5f98b7190d3cdf
musale <martinmshale@gmail.com>
2017-06-07 01:07:07 +0300

    Set up task file.

--- src/api/geofilterRectangle.js
+++ src/api/geofilterRectangle.js
@@ -1,3 +1,13 @@
+/**
+ Given a collection of N GPS coordinates about a user location L.
+ Return a subset S of coordinates from N that are within a rectangle of
+ width w and length l around the user’s location L. Such that;
+ when a client application makes an API request to request for coordinates,
+ it will provide a length l and width w in meters and a coordinate for an
+ arbitrary location L. The API should respond with coordinates within a
+ rectangle of the specified dimensions with L at the center of the rectangle.
+ */
+
 export default (req, res) => {
-  res.json({ url: '/api/v1/geofilter/rectangle' });
+    res.json({ url: '/api/v1/geofilter/rectangle' });
 };
